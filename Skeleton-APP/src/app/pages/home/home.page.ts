1a1deae6b626092d8b78858795892b65884d9513
chenaoh <al.lucerob@duocuc.cl>
2024-09-23 16:39:08 -0300

    logica incompleta del home

--- Skeleton-APP/src/app/pages/home/home.page.ts
+++ Skeleton-APP/src/app/pages/home/home.page.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  nombre !: string;
+  apellido !: string;
+
   constructor() { }
 
   ngOnInit() {
